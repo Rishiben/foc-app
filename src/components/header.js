c29f87333ff5ed93f8ea870e7d45ba0ac615d9b1
rishi ben <rishiben18@gmail.com>
2024-09-21 23:08:21 +0530

    i change

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Header.css';
 
 function Header() {
+    const handleClick = () => {
+        
+      };
   return (
       <header className="Header">
         <img src="https://res.cloudinary.com/dehnmebha/image/upload/v1726553806/FOC_LOFO_PNG-01_thmdjy.png" className="App-logo" alt="logo" />
         <p className='Para'>
         Welcome to FOC, your go-to destination for authentic street food delivery!
         </p>
+        <a className='feed' href='https://docs.google.com/forms/d/e/1FAIpQLSdvseFPxRJ53BSuXqNxDul5DQtUr6__MUIxU_fp1nq54MGLhg/viewform?usp=sf_link' target="_blank">Feedback</a>
       </header>
   );
 }
